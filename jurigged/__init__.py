608d3923b9c1c5124a88c2984757c39f19c0f889
Olivier Breuleux <breuleux@gmail.com>
2021-01-30 17:48:30 -0500

    Refactor recode.py a bit

--- jurigged/__init__.py
+++ jurigged/__init__.py
@@ -1,3 +1,4 @@
 from .codefile import CodeFile, Definition, conform, dig
 from .live import Watcher, watch
+from .recode import Recoder, virtual_file
 from .register import registry
--- jurigged/recode.py
+++ jurigged/recode.py
@@ -6,16 +6,19 @@ from .codefile import CodeFile, splitlines
 _count = count(1)
 
 
+def virtual_file(name, contents):
+    filename = f"<{name}#{next(_count)}>"
+    linecache.cache[filename] = (None, None, splitlines(contents), filename)
+    return filename
+
+
 class Recoder:
     def __init__(self, name, codefile):
         self.name = name
         self.codefile = codefile
-        # self.source = ""
-        # self.saved = False
 
     def patch(self, new_code):
-        filename = f"<{self.name}#{next(_count)}>"
-        linecache.cache[filename] = (None, None, splitlines(new_code), filename)
+        filename = virtual_file(self.name, new_code)
         cf = CodeFile(filename=filename, source=new_code)
         self.codefile.merge(cf, partial=True)
         self.source = new_code
