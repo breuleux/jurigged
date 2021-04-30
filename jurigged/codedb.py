42039bab12a5d827c28d4c517277832b83c02ab5
Olivier Breuleux <breuleux@gmail.com>
2021-04-30 00:54:35 -0400

    Rewrite audithook hooking

--- jurigged/codedb.py
+++ jurigged/codedb.py
@@ -3,13 +3,25 @@ import sys
 import types
 
 
+def make_audithook(self):
+    def watch_exec(event, obj):  # pragma: no cover
+        # Note: Python does not trace audit hooks, so normal use will not show
+        # coverage of this function even if it is executed
+        if event == "exec":
+            (code,) = obj
+            self.assimilate(code)
+
+    # The closure is faster than a method on _CodeDB
+    return watch_exec
+
+
 class _CodeDB:
     def __init__(self):
         self.codes = {}
 
     def setup(self):
         self.acquire_existing()
-        sys.addaudithook(self.audithook)
+        sys.addaudithook(make_audithook(self))
 
     def acquire_existing(self):
         for obj in gc.get_objects():
@@ -18,13 +30,6 @@ class _CodeDB:
                 self.assimilate(
                     co, (co.co_filename, *obj.__qualname__.split("."))
                 )
-
-    def audithook(self, event, obj):  # pragma: no cover
-        # Note: Python does not trace audit hooks, so normal use will not show
-        # coverage of this function even if it is executed
-        if event == "exec":
-            (code,) = obj
-            self.assimilate(code)
 
     def assimilate(self, code, path=()):
         if code.co_name == "<module>":  # pragma: no cover
