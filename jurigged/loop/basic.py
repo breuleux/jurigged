8e77c15e2a479fa21bfa6cb3683c40c829759e30
Olivier Breuleux <breuleux@gmail.com>
2021-11-04 16:48:57 -0400

    Fix read_chars of arrows/etc

--- jurigged/loop/basic.py
+++ jurigged/loop/basic.py
@@ -30,21 +30,29 @@ def read_chars():
         while True:
             ready, _, _ = select.select([sys.stdin], [], [], 0.02)
             if ready:
-                ch = sys.stdin.read(1)
-                if esc is not None:
-                    if ANSI_ESCAPE_INNER.match(ch):
-                        esc += ch
-                    elif ANSI_ESCAPE_END.match(ch):
-                        yield {"char": esc + ch, "escape": True}
-                        esc = None
+                # Sometimes, e.g. when pressing an up arrow, multiple
+                # characters are buffered, and read1() is the only way
+                # I found to read precisely what was buffered. select
+                # seems unreliable in these cases, probably because the
+                # buffer fools it into thinking there is nothing else
+                # to read. So read(1) would leave some characters dangling
+                # in the buffer until the next keypress.
+                for ch in sys.stdin.buffer.read1():
+                    ch = chr(ch)
+                    if esc is not None:
+                        if ANSI_ESCAPE_INNER.match(ch):
+                            esc += ch
+                        elif ANSI_ESCAPE_END.match(ch):
+                            yield {"char": esc + ch, "escape": True}
+                            esc = None
+                        else:
+                            yield {"char": esc, "escape": True}
+                            esc = None
+                            yield {"char": ch}
+                    elif ch == "\x1b":
+                        esc = ""
                     else:
-                        yield {"char": esc, "escape": True}
-                        esc = None
                         yield {"char": ch}
-                elif ch == "\x1b":
-                    esc = ""
-                else:
-                    yield {"char": ch}
     except Abort:
         pass
 
