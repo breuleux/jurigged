5a5e990bc4d160819335d7312e0cdb7fee5235ee
Olivier Breuleux <breuleux@gmail.com>
2021-11-04 15:39:19 -0400

    Improve loop interface

--- jurigged/loop/__init__.py
+++ jurigged/loop/__init__.py
@@ -1,17 +1,46 @@
 import builtins
-from functools import partial
+import functools
 from types import SimpleNamespace
 
 from giving import give, given
 
 from .basic import BasicDeveloopRunner
 from .develoop import Develoop, DeveloopRunner, RedirectDeveloopRunner
-from .richloop import RichDeveloopRunner
 
-loop = partial(Develoop, on_error=False, runner_class=RichDeveloopRunner)
-loop_on_error = partial(
-    Develoop, on_error=True, runner_class=RichDeveloopRunner
-)
+
+def keyword_decorator(deco):
+    """Wrap a decorator to optionally takes keyword arguments."""
+
+    @functools.wraps(deco)
+    def new_deco(fn=None, **kwargs):
+        if fn is None:
+
+            @functools.wraps(deco)
+            def newer_deco(fn):
+                return deco(fn, **kwargs)
+
+            return newer_deco
+        else:
+            return deco(fn, **kwargs)
+
+    return new_deco
+
+
+@keyword_decorator
+def loop(fn, interface="rich", only_on_error=False):
+    if interface == "rich":
+        from .richloop import RichDeveloopRunner
+
+        interface = RichDeveloopRunner
+    elif interface == "basic":
+        interface = BasicDeveloopRunner
+    elif isinstance(interface, str):
+        raise Exception(f"Unknown develoop interface: '{interface}'")
+
+    return Develoop(fn, on_error=only_on_error, runner_class=interface)
+
+
+loop_on_error = functools.partial(loop, only_on_error=True)
 xloop = loop_on_error
 
 __ = SimpleNamespace(
